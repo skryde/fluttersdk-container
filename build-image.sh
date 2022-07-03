ec446d8e42b6d7a39c19c915a61592f95deec6d3
Sebastian Rodriguez <sebart.07@gmail.com>
2022-07-03 00:08:46 -0300

    Modified image building script to get Username from 'docker info' command.

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-imageName="skryde/fluttersdk"
+username=`docker info | grep -i username | awk -F ": " '{ print $2 }'`
+if [[ -z $username ]]; then
+	echo "You must login into Docker Hub Resgistry before continue..."
+	exit 1
+fi
+
+image="fluttersdk"
+imageName="$username/$image"
 
 docker build --network=host -t $imageName .
 if [[ $? -ne 0  ]]; then
